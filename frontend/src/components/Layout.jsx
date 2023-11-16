6388d7156680d2dfa46211a7f001adde518d2b6a
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-11-16 14:12:36 +0100

    just copyediting

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,14 +5,14 @@ const Layout = () => {
     <div className="layout">
         <header>
             <nav className="bg-black text-white text-xs">
-                 <NavLink to="/">Home</NavLink> | 
+                 <NavLink to="/">| Home</NavLink> | 
                 | <NavLink to="templates">Templates</NavLink>  | 
                 | <NavLink to="dashboard">Dashboard</NavLink>  | 
                 | <NavLink to="controller">Controller</NavLink>  | 
                 | <NavLink to="beamer">Beamer</NavLink>  | 
                 | <NavLink to="about">About</NavLink>  | 
                 | <NavLink to="error">Error</NavLink>
-                  <span>← Navigation for development purposes only.</span>
+                  <span> | ← Navigation for development purposes only.</span>
             </nav>
         </header>
         <main><Outlet/></main>
