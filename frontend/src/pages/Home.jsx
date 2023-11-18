a598d02c249b626a74990d34c6c3c0bb46419c8e
lucy444sarah <hagedornalvarezsully@gmail.com>
2023-11-18 09:18:40 +0100

    Add content in landing page,colors to be defined yet

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,33 @@
 export const Home = () => {
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col items-center min-h-screen bg-A26769 text-6D2E46">
+      <div className="flex justify-between items-center w-full p-4">
+       
+
+      {/* for logo */}
+        <img src="./src/assets/images/logo.png" alt="Logo" className="max-w-32" />
+
+        
+        <div className="flex gap-4">
+          <button className="px-4 py-2 bg-rose-400 text-pink-950 rounded">Sign Up</button>
+          <button className="px-4 py-2 bg-rose-400 text-pink-950 rounded">Already have an account? Log In</button>
+        </div>
+      </div>
+
+      
+      <div className="flex flex-col items-center mt-16">
+        <button className="px-8 py-4 bg-rose-400 text-pink-950 rounded mb-4">Create your first stage</button>
+        <button className="px-8 py-4 bg-rose-400 text-pink-950 rounded mb-4">Choose a template</button>
+        <button className="px-8 py-4 bg-rose-400 text-pink-950 rounded mb-4">Watch tutorial video</button>
+      </div>
+
+      
+      <div className="fixed bottom-8 right-8">
+        <button className="p-4  bg-rose-400  text-pink-950 rounded-full">Share</button>
+      </div>
+    </div>
+  );
+};
+
+
