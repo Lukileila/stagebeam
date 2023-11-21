ed3a07baf733de304606c9fa50d4350eabe87120
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-11-21 16:06:59 +0100

    just saving...

--- frontend/src/pages/Controller.jsx
+++ frontend/src/pages/Controller.jsx
@@ -3,13 +3,23 @@
 import { ObjectsMenu } from "../components/ObjectsMenu"
 import { Timeline } from "../components/Timeline"
 import { Workspace } from "../components/Workspace"
+import { useEffect, useState } from "react";
+import templateObjects from "../data/objectTemplates.json"
+
+
 
 export const Controller = () => {
+
+  // Active Objects: They are aaaaaaaall going to live here:
+  const [activeObjects, setActiveObjects] = useState(/* templateObjects.json() */);
+
+  useEffect(() => { console.log(templateObjects) }, []);
+
   return (
     <>
-        <ObjectsMenu/>
+        <ObjectsMenu activeObjects={activeObjects} setActiveObjects={setActiveObjects}/>
         <Timeline/>
-        <Workspace/> {/*  order matters for overlap, ignoring the set z-index. Yes. Really. I hate it as well. /LZ */}
+        <Workspace activeObjects={activeObjects} setActiveObjects={setActiveObjects}/> {/*  order matters for overlap, ignoring the set z-index. Yes. Really. I hate it as well. /LZ */}
     </>
           )
 }
