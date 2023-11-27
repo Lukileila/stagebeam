cf4533ca5de8215dd24f53b268c312b773620bde
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-11-27 17:21:30 +0100

    more work on the Object Menu

---
+++ frontend/src/components/ActiveCard.jsx
@@ -0,0 +1,27 @@
+export const ActiveCard = ({templateObjects, setTemplateObjects, activeObjects, setActiveObjects}) => {
+
+
+    const addToActive = (templateObject) => {
+        setActiveObjects(prev => [...prev, {...templateObject, id: `${templateObject.name}_${crypto.randomUUID()}`}]);
+      };
+  return (
+
+
+
+    <>
+
+
+                {templateObjects.length>0 && templateObjects.map((templateObject,it)=>{
+                    return <div key={it} onClick={()=>addToActive(templateObject)} className="cursor-pointer">
+                                <div className='w-10 h-14 border border-slate-800 bg-gray-950 rounded m-px'>
+                                    <img className="w-10 aspect-square"src={`${templateObject.thumbnail}`} alt="img" />
+                                    <p className='text-xs truncate text-ellipsis h-10 '>{templateObject.name}</p>
+                                </div>
+                        </div>
+                })}
+
+
+    
+    </>
+  )
+}
--- frontend/src/components/ObjectsMenu.jsx
+++ frontend/src/components/ObjectsMenu.jsx
@@ -1,35 +1,66 @@
 import React from 'react';
 import { useEffect, useState } from "react";
+import { PaletteCard } from "./PaletteCard";
 
 export const ObjectsMenu = ({templateObjects, activeObjects, setActiveObjects}) => {
 
-  const addToActive = (templateObject) => {
-    setActiveObjects(prev => [...prev, {...templateObject, id: `${templateObject.name}_${crypto.randomUUID()}`}]);
-  };
+  
+  const [beamerOnline, setBeamerOnline] = useState( JSON.parse(localStorage.getItem('beamerOnline'))) 
+
+
+  useEffect(() => {
+    const onStorageUpdate=()=>{ setBeamerOnline(JSON.parse(localStorage.getItem('beamerOnline')) ?? false)}
+    
+    window.addEventListener('storage', onStorageUpdate);
+    return () => {
+    window.removeEventListener('storage', onStorageUpdate);
+    };
+  }, []);
+
+
 
   const openBeamer = ()=> {
     window.open("/beamer")
   }
 
+  const clearActiveObjects = ()=>{
+    setActiveObjects([])
+  }
+
+
   return (
     <>
       <div className='flex flex-col fixed w-[30%] h-full bg-black p-1 pr-0'>
-        <div   className='flex justify-between border-2 border-gray-800 bg-gradient-to-t from-gray-950 to-gray-900  rounded p-1 my-1 text-gray-300  cursor-pointer' > 
-          <h1 className='text-gray-600'>Beamer window<span> is open</span></h1>
-          <div className="border-2 border-gray-700 bg-gray-800   rounded p-1 my-1 text-gray-600 cursor-pointer"> 
-          <p>test element</p></div>
-          <button onClick={openBeamer} className="border-2 border-gray-800 bg-gradient-to-b from-gray-700 hover:from-gray-900 to-gray-600 hover:to-gray-800 rounded p-1 my-1">
-            <img src="./src/assets/icons/popoutSvg.svg" alt="popout" className="w-5 aspect-square fill-white"></img>
+        <div   className='flex flex-col justify-between border-2 border-gray-800 bg-gradient-to-t from-gray-950 to-gray-900 rounded p-1 text-gray-300  cursor-pointer' > 
+          <h1 className='text-gray-600'>Beamer window</h1>
+
+          <button onClick={openBeamer} className=' border-2 border-gray-800  rounded p-1 ' style={{backgroundColor:beamerOnline?'#FACC15':"#6B7280"}}>
+            <p className='inline text-gray-900'>{beamerOnline?"beamer window is open ":'pop out the beamer window '}</p><img src="./src/assets/icons/popoutSvg.svg" alt="popout" className=" inline w-5 aspect-square fill-white"></img>
           </button>
         </div>     
-        <div className='grow border-2 border-gray-800 bg-gradient-to-t from-gray-950 to-gray-900 rounded p-1  text-gray-300 z-0'> 
-        <h1 className='text-gray-600'>Objects</h1>
-        <ul className='list-disc p-5'>
-          {templateObjects.length>0 && templateObjects.map((templateObject,it)=>{
-            return <li key={it} onClick={()=>addToActive(templateObject)} className="cursor-pointer">{templateObject.name}</li>
-          })}
-        </ul>
-      </div>
+
+        <div className='grow flex flex-col border-2 my-1 border-gray-800 bg-gradient-to-t from-gray-950 to-gray-900 rounded p-1  text-gray-300 z-0'> 
+          <h1 className='text-gray-600'>Objects Palette</h1>
+          <div className='grow flex flex-row flex-wrap justify-start content-start border-2 my-1 border-gray-950 bg-black rounded-lg p-1  text-gray-300 z-0'> 
+              <PaletteCard templateObjects={templateObjects} activeObjects={activeObjects} setActiveObjects={setActiveObjects}/>
+          </div>
+        </div>
+        <div className='grow flex flex-col border-2  border-gray-800 bg-gradient-to-t from-gray-950 to-gray-900 rounded p-1  text-gray-300 z-0'> 
+          <div className="flex flex-row justify-between">
+          <h1 className='text-gray-600'>Active Objects</h1>
+          <button onClick={clearActiveObjects} className=' border-2 border-gray-800 bg-gray-900 text-white rounded px-1 h-'>
+            <p className='inline text-gray-600 '>clear all 🗑️</p>  </button>
+          </div>
+
+          <div className='grow border-2 my-1 border-gray-950 bg-black  rounded-lg px-1  text-gray-300 z-0'> 
+            <ul className='list-disc p-5'>
+              {activeObjects.length>0 && activeObjects.map((Object,it)=>{
+                return <li key={it} onClick={()=>addToActive(Object)} className="cursor-pointer">{Object.name}</li>
+              })}
+            </ul>
+          </div>
+          
+        </div>
  
       </div>
     </>
