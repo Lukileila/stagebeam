f7aec5b7978802a1ab58ab0fa3b252ca37fcd2d8
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-11-29 15:44:40 +0100

    copied parts of pbjectcreator to objectcreatorbeamer

--- frontend/src/components/ObjectCreatorBeamer.jsx
+++ frontend/src/components/ObjectCreatorBeamer.jsx
@@ -2,31 +2,29 @@ export const ObjectCreatorBeamer = ({activeObjects}) => {
 
   return (<>
   
-  
     {activeObjects.length>0 && activeObjects.map((x,i)=>{
       return (
           
           <div key={x.id} id={x.id}
-            className="absolute block cursor-grab animate-spin mix-blend-screen" 
+            className="absolute block cursor-grab mix-blend-screen" 
             style={{
               top: x.position.ry*100 + '%',
               left: x.position.rx*100 + '%',
             }}>
             
-
-            {x.elements.length>0 && x.elements.map((x,j)=>{ return (
+            {x.elements.length>0 && x.elements.map((element,j)=>{ return (
             <div key={j}
               className="absolute block text-white mix-blend-screen cursor-grab"  
               style={{
-                width:x.size*screen.width+'px',
-                aspectRatio:x.css.aspectRatio,
-                borderRadius:x.css.borderRadius,
-                backgroundColor:x.css.backgroundColor,
-                translate:x.css.translate
+                width:element.size*x.size*screen.width+'px',
+                aspectRatio:element.css.aspectRatio,
+                borderRadius:element.css.borderRadius,
+                backgroundColor:element.css.backgroundColor,
+                translate:element.css.translate,
+                opacity: x.opacity,
               }}
-            >{x.name}</div>
+            ></div>
             )})}
-
           </div>
         
 
